0c698e921cacf7ed96ed5eed06dd28a7814cf85a
Triplerush <fcanal@unsa.edu.pe>
2024-06-24 21:40:16 -0500

    Agregando el archivo sql para generar las tablas como el codigo No rollback del ejercicio resuelto

---
+++ EjercicioResuelto/Script.sql
@@ -0,0 +1,12 @@
+-- Estructura de tabla para la tabla 'mitabla'
+CREATE TABLE mitabla (
+    DNI varchar(12) DEFAULT NULL,
+    correo varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Estructura de tabla para la tabla 'miotratabla'
+CREATE TABLE miotratabla (
+    nombre varchar(20) DEFAULT NULL,
+    apellido varchar(20) DEFAULT NULL,
+    edad int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
